ea1fb7c0af5a39e301d865c262686a2e7ebfb21e
Paul Crowley <paul@lshift.net>
2012-12-14 17:21:46 +0000

    Rename the project, add midje and json stuff

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
-(defproject helloworld "1.0.0-SNAPSHOT"
+(defproject eventframework "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.1"]
                  [ring/ring-devel "1.1.0"]
-                 [aleph "0.2.2"]]
+                 [aleph "0.2.2"]
+                 [ring/ring-json "0.1.2"]
+                 [midje "1.4.0"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]]
-  :hooks [environ.leiningen.hooks])
+  :hooks [environ.leiningen.hooks]
+  :profiles {
+    :dev {:dependencies [[ring-mock "0.1.3"]]
+          :plugins [[lein-midje "2.0.2"]]}})
